2931c5ebfffbff9ebbcb2cbc36763d1b8c626e2f
Gabriela Trindade <byzinha88@hotmail.com>
2021-09-16 00:26:26 -0300

    desenvolvimento da tela de agendamentos feitos

--- welcome/welcome.js
+++ welcome/welcome.js
@@ -66,3 +66,80 @@ const carregaNomeUsuario = () => {
 }
 
 carregaNomeUsuario()
+
+/* consulta das reservas */
+
+const createEl = (type, className) => {
+  const elem = document.createElement(type);
+  elem.onclick = {}
+  if(className !== null && className !== '' && className !== "")
+    elem.classList.add(className);
+  return elem;
+}
+
+const createReservasRealizadas = (reserva, numero) => {
+
+  const card = createEl('div', 'card');
+  card.id = `card_hidden_${numero}`;
+  const html = `<div class="card-container">
+      <div class="date">
+        <div class="menu-more">
+          <div class="dropdown">
+            <button onclick="myFunction()" class="dropbtn"></button>
+            <div id="myDropdown" class="dropdown-content">
+              <a href="#home"
+                ><span class="icon-TypeEdit"></span> Editar</a
+              >
+              <a href="#about"
+                ><span class="icon-TypeDelete"></span> Apagar</a
+              >
+            </div>
+          </div>
+        </div>
+        <div class="information">
+          <label id="data_reserva">${reserva.data_reserva.replaceAll("-","/")}</label>
+        </div>
+      </div>
+      <div class="divider-1"></div>
+      <div class="section">
+        <div class="information">
+          <i class="icon-TypePlace" id="icon-confirmation"></i>
+          <h4 id="unidade_negocio">${reserva.nome_unidade_negocio}</h4>
+        </div>
+
+        <div class="information">
+          <i class="icon-TypeWork" id="icon-confirmation"></i>
+          <h4 id="estacao_trabalho">${reserva.nome_estacao_trabalho}</h4>
+        </div>
+      </div>
+    </div>`
+    card.innerHTML = html;
+    return card;
+}
+
+const getReservasDisponiveis = async () => {
+
+  const id_usuario = localStorage.getItem('id');
+  console.log(id_usuario + "euuuu");
+
+  const response = await fetch(`https://orangepoint.herokuapp.com/reservas/${id_usuario}`, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json'
+      }
+  });
+
+  const myJson = await response.json();
+  console.log(myJson);
+  if(myJson != null && myJson != undefined && myJson.length > 0) {
+    document.getElementById('sem_agendamentos').style.display = "none";
+    for (var index in myJson) {
+      document.getElementById("reservas_realizadas").appendChild(createReservasRealizadas(myJson[index], index))
+    }
+  } else {
+    document.getElementById('sem_agendamentos').style.display = "block";
+  }
+  
+}
+
+getReservasDisponiveis();
